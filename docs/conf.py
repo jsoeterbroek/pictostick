872ca0083aa854506bd63b0ceafbe68b504b18ae
Joost Soeterbroek <j.soeterbroek@dictu.nl>
2025-03-31 21:17:04 +0200

    minor

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = '1.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx_rtd_dark_mode"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
