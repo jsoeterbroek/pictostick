b4545244d3d4ce304bed6394d373b6e28c5b67d9
Joost Soeterbroek <j.soeterbroek@dictu.nl>
2025-04-03 15:50:39 +0200

    add logo

--- docs/conf.py
+++ docs/conf.py
@@ -24,5 +24,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+#-html_theme = 'alabaster'
+
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_logo = "logo200x120.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
