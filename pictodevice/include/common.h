a18057cd0a68b3c42b94cb8d6a2305ee0ba7574e
Joost Soeterbroek <j.soeterbroek@dictu.nl>
2025-04-03 09:54:51 +0200

    minor

--- pictodevice/include/common.h
+++ pictodevice/include/common.h
@@ -1,5 +1,4 @@
 #include <ArduinoJson.h>
-//#include <pins_arduino.h>
 #include <colors.h>
 
 #define PD_VERSION_MAJOR 1
@@ -15,8 +14,6 @@ static inline int pd_version_minor(void) {
 static inline int pd_version_patch(void) {
     return PD_VERSION_PATCH;
 }
-
-//int counter=0;
 
 // status flags
 boolean STATUS_WIFI_OK = false;
@@ -39,7 +36,7 @@ const char* ntpServer = "europe.pool.ntp.org";
 const char* wifi_mngr_networkname = "pictodevice";
 const char* wifi_mngr_password = "p@ssw0rd";
 
-// 192.168.178.148 FIXME:  servername for HTTP server needs to change
+// FIXME: servername for HTTP server needs to change
 String serverName = "http://192.168.178.148:8001";
 
 #define FORMAT_SPIFFS_IF_FAILED true
@@ -52,10 +49,6 @@ const char* config_device_ip; // "128.8.2.123"
 const char* config_date_created; // "24-03-2025"
 const char* config_date_valid; // "23-03-2025"
 int8_t config_activities_size = 0;
-//int config_activities[0];
-//String config_activities_name_nl[];   //   "name_nl": "douchen"
-//int config_activities_order[];        //   "order": "002",
-//String config_activities_picto[];     //   "picto": "shower.png",
 int config_activities_size_max = 19;   // hardcoded maximum of activities still able to show mini menu
 
 // box for picto 100 x 100 pixels 
@@ -82,6 +75,7 @@ int setMax[8]={24,60,60,32,13,36,2,2};
 int setPosX[8]={10,50,91,10,50,91,8,8};
 int setPosY[8]={54,54,54,124,124,124,172,192};
 int chosen=0;
+
 //brightness and battery
 int brightness[6] = {16, 32, 48, 64, 96, 180};
 int b = 2;
@@ -93,15 +87,15 @@ int buzzer = 1;
 
 //sleep variables
 int sleepTime = 50;
-int ts,tts = 0;
+int ts, tts = 0;
 bool slp = false;
 
 #define BUTTON_PRESSED LOW 
 #define BUTTON_RELEASED HIGH
 
-uint16_t ontime, offtime;
-uint8_t i,num_codes;
-uint8_t region;
+//uint16_t ontime, offtime;
+//uint8_t i, num_codes;
+//uint8_t region;
 
 #define MAX_IMAGE_WIDTH 100 // Adjust for your images
 int16_t xpos = 12;
@@ -109,5 +103,5 @@ int16_t ypos = 12;
 int current_activity_index = 0;
 int ps_current_activity_index = 0;
 
-String lang = "en"; // "en" for english, "nl" for dutch
+String lang = "nl"; // "en" for english, "nl" for dutch
 bool marked_done = false;
