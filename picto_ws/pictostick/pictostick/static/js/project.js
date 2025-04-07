844d3df778bf637aa29ebc8b24accdf1ee2ba579
Joost Soeterbroek <j.soeterbroek@dictu.nl>
2025-04-07 16:33:45 +0200

    add get_assign trigger

--- picto_ws/pictostick/pictostick/static/js/project.js
+++ picto_ws/pictostick/pictostick/static/js/project.js
@@ -1,3 +1,7 @@
+var activity_id= '0';
+var list_id= '0';
+
+
 new Sortable(avgrid, {
   group: {
       name: 'shared',
@@ -9,20 +13,28 @@ new Sortable(avgrid, {
   sort: false // To disable sorting: set sort to false
 });
 
+//    get_assign(activity_id, list_id);
 new Sortable(fugrid, {
   group: {
       name: 'shared',
       pull: 'clone',
   },
   animation: 150,
-  ghostClass: 'blue-background-class',
+  ghostClass: 'blue-background-class'
 });
-
 
 new Sortable(Monday, {
-  group: { name: 'shared', pull: 'clone' },
-  animation: 150, ghostClass: 'blue-background-class'
+  group: { 
+    name: 'shared', 
+    pull: 'clone' 
+  },
+  animation: 150, 
+  ghostClass: 'blue-background-class',
+  onUpdate: function (evt) {
+    get_assign(activity_id, list_id);
+  }
 });
+
 new Sortable(Tuesday, {
   group: { name: 'shared', pull: 'clone' },
   animation: 150, ghostClass: 'blue-background-class'
@@ -47,3 +59,21 @@ new Sortable(Sunday, {
   group: { name: 'shared', pull: 'clone' },
   animation: 150, ghostClass: 'blue-background-class'
 });
+
+function get_assign(activity_id, list_id){
+  console.log('get_assign triggered');
+  //send_request(activity_id, list_id);
+}
+
+function send_request(activity_id, list_id){
+  $.ajax({
+    type: 'GET',
+    url: `/myapp/task-assign/${list_id}/${activity_id}/`,
+    failure: function(data){
+      console.log('failure');
+      console.log(data);
+    },
+  });
+}
+// Inspired by https://github.com/TomIsLoading/drag-and-drop-kanban
+// Inspired by https://medium.com/@jakubjadczak02/drag-drop-and-django-5a5743913c8f
