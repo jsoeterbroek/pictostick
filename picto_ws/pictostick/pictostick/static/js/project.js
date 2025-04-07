1568610e6608bcc0691762527a7615cff0253ad4
Joost Soeterbroek <j.soeterbroek@dictu.nl>
2025-04-07 12:12:33 +0200

    add grid view

--- picto_ws/pictostick/pictostick/static/js/project.js
+++ picto_ws/pictostick/pictostick/static/js/project.js
@@ -1,15 +1,40 @@
-new Sortable(pictos, {
+new Sortable(grid, {
   group: {
       name: 'shared',
-      pull: 'clone'
+      pull: 'clone',
+      put: false // Do not allow items to be put into this list
   },
-  animation: 150
+  animation: 150,
+  ghostClass: 'blue-background-class',
+  sort: false // To disable sorting: set sort to false
 });
 
-new Sortable(right, {
-  group: {
-      name: 'shared',
-      pull: 'clone'
-  },
-  animation: 150
+
+new Sortable(Mon, {
+  group: { name: 'shared', pull: 'clone' },
+  animation: 150, ghostClass: 'blue-background-class'
+});
+new Sortable(Tue, {
+  group: { name: 'shared', pull: 'clone' },
+  animation: 150, ghostClass: 'blue-background-class'
+});
+new Sortable(Wed, {
+  group: { name: 'shared', pull: 'clone' },
+  animation: 150, ghostClass: 'blue-background-class'
+});
+new Sortable(Thu, {
+  group: { name: 'shared', pull: 'clone' },
+  animation: 150, ghostClass: 'blue-background-class'
+});
+new Sortable(Fri, {
+  group: { name: 'shared', pull: 'clone' },
+  animation: 150, ghostClass: 'blue-background-class'
+});
+new Sortable(Sat, {
+  group: { name: 'shared', pull: 'clone' },
+  animation: 150, ghostClass: 'blue-background-class'
+});
+new Sortable(Sun, {
+  group: { name: 'shared', pull: 'clone' },
+  animation: 150, ghostClass: 'blue-background-class'
 });
